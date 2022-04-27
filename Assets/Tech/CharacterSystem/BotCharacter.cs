920112110c9122515f7daa9c300505d423325dd2
Alexey Kavin <smillerlenatderirinc@gmail.com>
2022-04-27 19:40:35 +0300

    behaviour tree addon

---
+++ Assets/Tech/CharacterSystem/BotCharacter.cs
@@ -0,0 +1,13 @@
+﻿using ECS.Mono;
+
+namespace CharacterSystem
+{
+    public class BotCharacter : MonoGameObjectEntity
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            Entity.isCharacter = true;
+        }
+    }
+}
